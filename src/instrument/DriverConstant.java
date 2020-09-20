e575576e59a7f2a71eab1c0ad3476fbb506386be
vietth2004 <vietth2004@gmail.com>
2020-09-20 15:14:26 +0700

    Finish coding instrument

--- src/instrument/DriverConstant.java
+++ src/instrument/DriverConstant.java
@@ -5,29 +5,29 @@ public interface DriverConstant
 
     String ASSERT_ENABLE = "ASSERT_ENABLE";
 
-    String CALL_COUNTER = "AKA_fCall";
+    String CALL_COUNTER = "UET_fCall";
 
-    String TEST_NAME = "AKA_test_case_name";
+    String TEST_NAME = "UET_test_case_name";
 
     String TEST_PATH_FILE_TAG = "{{INSERT_PATH_OF_TEST_PATH_HERE}}";
 
-    String RUN_TEST = "AKA_run_test";
+    String RUN_TEST = "UET_run_test";
 
-    String MARK = "AKA_mark";
+    String MARK = "UET_mark";
 
     String EXEC_TRACE_FILE_TAG = "{{INSERT_PATH_OF_EXE_RESULT_HERE}}";
 
-    String ASSERT_METHOD = "AKA_assert_method";
+    String ASSERT_METHOD = "UET_assert_method";
 
-    String ASSERT_DOUBLE_METHOD = "AKA_assert_double_method";
+    String ASSERT_DOUBLE_METHOD = "UET_assert_double_method";
 
-    String ASSERT_PTR_METHOD = "AKA_assert_ptr_method";
+    String ASSERT_PTR_METHOD = "UET_assert_ptr_method";
 
-    String ASSERT = "AKA_assert";
+    String ASSERT = "UET_assert";
 
-    String ASSERT_DOUBLE = "AKA_assert_double";
+    String ASSERT_DOUBLE = "UET_assert_double";
 
-    String ASSERT_PTR = "AKA_assert_ptr";
+    String ASSERT_PTR = "UET_assert_ptr";
 
     String ADDITIONAL_HEADERS_TAG = "/*{{INSERT_ADDITIONAL_HEADER_HERE}}*/";
 
